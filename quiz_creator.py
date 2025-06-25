0abe13b98139bd5d216ef1cb42498bf3e9797212
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-25 22:54:30 +0800

    Added a dictionary for answers and added a for loop statement for a list containing options and added a variable for the input of the answer for each option and set the answer for each option in the dictionary for answers

--- quiz_creator.py
+++ quiz_creator.py
@@ -3,3 +3,8 @@ class QuizCreator():
     def create_quiz(self):
         while True:
         	question = input("Enter a question:\n")
+
+            answers = {}
+            for option in ['a', 'b', 'c', 'd']:
+                answer = input(f"Enter answer option {option}:\n")
+                answers[option] = answer
