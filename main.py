36fd685e8585a99b5cbafafada838df7fd508bc8
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-30 00:06:45 +0800

    Moved the load quiz function and its class call and its function calls from the quiz module to the main module

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import quiz
+
+def load_quiz(quiz_file):
+	quiz1 = Quiz(quiz_file)
+	quiz1.load_question()
+	quiz1.load_answers()
+	correct_answer = quiz1.load_correct_answer()
+	your_answer = quiz1.input_your_answer()
+	quiz1.check_if_your_answer_is_correct(your_answer, correct_answer)
+
+load_quiz("quiz.txt")
--- quiz.py
+++ quiz.py
@@ -40,13 +40,3 @@ class Quiz():
 		else:
 			print("Your answer is incorrect!")
 			print(f"Correct answer: {self.correct_answer}")
-
-def load_quiz(quiz_file):
-	quiz1 = Quiz(quiz_file)
-	quiz1.load_question()
-	quiz1.load_answers()
-	correct_answer = quiz1.load_correct_answer()
-	your_answer = quiz1.input_your_answer()
-	quiz1.check_if_your_answer_is_correct(your_answer, correct_answer)
-
-load_quiz("quiz.txt")
