3920c4709be6d77390f38e34656cdb99e14b1b65
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-29 11:52:30 +0800

    Created a random number from the identified number of entries

--- quiz.py
+++ quiz.py
@@ -6,3 +6,4 @@ class Quiz():
 		with open(quiz_file, "r") as file:
 			self.lines = file.readlines()
 		self.number_of_entries = int(len(self.lines) / 6)
+		self.random_number = random.randrange(1, self.number_of_entries + 1)
