00a9f19f98bdedf6fb821324a31bfc7c6bfd89f2
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2025-06-29 12:49:42 +0800

    Loaded the text file

--- quiz.py
+++ quiz.py
@@ -48,3 +48,5 @@ def load_quiz(quiz_file):
 	correct_answer = quiz1.load_correct_answer()
 	your_answer = quiz1.input_your_answer()
 	quiz1.check_if_your_answer_is_correct(your_answer, correct_answer)
+
+load_quiz("quiz.txt")
